911cf85d5ed4ed7063ab47e655867718a0cf8811
umfb <abdulbasitadebajo1619@gmail.com>
2025-03-11 15:06:20 +0100

    removed bug

--- src/components/Dropdown.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dropdown({ array }: { array: Array<string> }) {
-  return (
-    <ul role="listbox">
-      {array.map((item, index) => (
-        <li>{item}</li>
-      ))}
-    </ul>
-  );
-}
